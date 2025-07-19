5a9b2c2e75bd4269706f64c9efbad81d6eb79320
Sara Garzou <sgarzou@proton.me>
2025-07-19 12:58:57 +0200

    Fix requirements

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import base64
 import io
 from PIL import Image
 
-from backend.nn import NeuralNetwork
+from .nn import NeuralNetwork
 
 model = NeuralNetwork((784, 128, 64, 10))
 model.load_weights("model_weights.npz")
